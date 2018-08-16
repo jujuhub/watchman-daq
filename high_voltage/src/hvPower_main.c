8f1e7de02fcaa7e3b42d81e0bbe0824cc2276f69
Julie He <juhe@ucdavis.edu>
2018-08-15 17:54:41 -0700

    Updates - Aug 15 2018

--- high_voltage/src/hvPower_main.c
+++ high_voltage/src/hvPower_main.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define UNIX
 #include <CAENHVWrapper.h>
@@ -12,15 +13,24 @@ int main(int argc, char *argv[])
 
 	int res = -1, toggle = 0;
 
-	if (argc != 2) { printf("Number of arguments given does not match number of arguments expected (2).\n"); }
-	else { toggle = (int)argv[1]; } // #TODO need to figure out how to properly cast / incorporate passing str arg into shell script / 2 if statements with or conditions
+	if (argc != 2)
+	{
+		printf("Number of args given does not match number expected [2].\n");
+		exit(1);
+	}
+	else if (strcmp(argv[1], "ON") == 0) { toggle = 1; }
+	else if (strcmp(argv[1], "OFF") == 0) { toggle = 0; }
+
+	printf("toggle = %d\n", toggle);
 
 	for (ushort chNum = 0; chNum <= 3; chNum++)
 	{
 		res = togglePower(chNum, toggle);
-		if (res == 0 && toggle = 1) { printf("CH %d: Successfully turned ON\n", chNum); }
-		else if (res == 0 && toggle = 0) { printf("CH %d: Successfully turned OFF\n", chNum); }
-		else { printf("ERROR: Unsuccessful communication with power supply"); }
+		sleep(2);
+//		printf("res = %d\n", res);
+		if (res == CAENHV_OK && toggle == 1) { printf("CH %d: Successfully turned ON\n", chNum); }
+		else if (res == CAENHV_OK && toggle == 0) { printf("CH %d: Successfully turned OFF\n", chNum); }
+		else { printf("ERROR: Unsuccessful communication with power supply\n"); }
 	}
 
 	return res;
